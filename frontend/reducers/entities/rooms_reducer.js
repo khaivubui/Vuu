6214fa3a3e189ff8d97bb644c19571ae26d9096a
Khai Bui <khaivubui@gmail.com>
2017-09-25 11:39:46 -0700

    Fix the rooms persisting after log out bug

--- frontend/reducers/entities/rooms_reducer.js
+++ frontend/reducers/entities/rooms_reducer.js
@@ -7,7 +7,7 @@ const roomsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ROOMS_AND_USERS:
-      return action.rooms || state;
+      return action.rooms || {};
     case RECEIVE_ROOM:
       return merge({}, state, {[action.room.id]: action.room});
     default:
