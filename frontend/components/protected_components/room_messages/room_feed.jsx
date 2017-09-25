97d0a4aebea133f032057a85faa2fa3c0aa3f151
Khai Bui <khaivubui@gmail.com>
2017-09-24 22:41:14 -0700

    Add fetching messages for dms on DidMount and render them;

--- frontend/components/protected_components/room_messages/room_feed.jsx
+++ frontend/components/protected_components/room_messages/room_feed.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 
+import MessageContainer from '../message_container';
+
 export default class RoomFeed extends React.Component {
+  componentDidMount () {
+    this.props.fetchRoomMessagesWithUsers(
+      this.props.match.params.roomId
+    ).then(() => this.refreshScroll());
+  }
+
+  refreshScroll () {
+    this.refs.messages.scrollTop = this.refs.messages.scrollHeight;
+  }
+
   render () {
-    return (
-      <div className='feed'>
-        LOL
-      </div>
+    const { room } = this.props;
+    const { users } = this.props;
+    const messages = this.props.messages[0] && this.props.messages.map(
+      message => <MessageContainer key={message.id}
+                          user={users[message.userId]}
+                          message={message}/>
     );
+
+    if (room) {
+      return (
+        <div className='feed'>
+          <div className='context-information'>
+            <h1>BLAHHHH</h1>
+            #BLAHHH
+          </div>
+          <ul className='messages' ref='messages'>
+            {messages}
+          </ul>
+        </div>
+      );
+    } else {
+      // When just refreshed and room isn't fetched yet.
+      // Not really necessary, now that we have RestrictedRoute
+      return (
+        <div className='feed'>
+        </div>
+      );
+    }
   }
 }
