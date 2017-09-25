d9737a3cba2d649c5570d65a02eeb080bfccca37
Khai Bui <khaivubui@gmail.com>
2017-09-25 14:14:42 -0700

    Touch up styling

--- frontend/components/protected_components/message.jsx
+++ frontend/components/protected_components/message.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default ({ user, message, openUserShow }) =>
-  <li className='message'>
+export default ({ user, message, openUserShow, isCurrentUser }) =>
+  <li className={isCurrentUser ? 'message is-current-user' : 'message'}>
     <span className='user' onClick={() => openUserShow(user.id)}>
       {user.displayname || user.username}
     </span>
--- frontend/components/protected_components/message_container.js
+++ frontend/components/protected_components/message_container.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { openUserShow } from '../../actions/ui/user_show_actions';
 import Message from './message';
 
-const mapStateToProps = state => ({
-
+const mapStateToProps = (state, ownProps) => ({
+  isCurrentUser: ownProps.user.id === state.session.currentUser.id
 });
 
 const mapDispatchToProps = dispatch => ({
