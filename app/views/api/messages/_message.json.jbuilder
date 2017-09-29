35a53f5a3ee78145f01321bcf0546bf5d1dc219a
Khai Bui <khaivubui@gmail.com>
2017-09-29 13:26:22 -0700

    Add timestamps

--- app/views/api/messages/_message.json.jbuilder
+++ app/views/api/messages/_message.json.jbuilder
@@ -2,4 +2,6 @@ json.extract! message, :id,
                        :body,
                        :user_id,
                        :context_type,
-                       :context_id
+                       :context_id,
+                       :created_at,
+                       :updated_at
--- frontend/components/protected_components/message.jsx
+++ frontend/components/protected_components/message.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default ({ user, message, openUserShow, isNew }) =>
   <li className={isNew ? 'message is-new' : 'message'}>
-    <span className='user' onClick={() => openUserShow(user.id)}>
-      {user.displayname || user.username}
-    </span>
-    : {message.body}
+    <div>
+      <span className='user' onClick={() => openUserShow(user.id)}>
+        {user.displayname || user.username}
+      </span>
+      : {message.body}
+    </div>
+    <div className='timestamp'>
+      {new Date(message.createdAt).toLocaleString()}
+    </div>
   </li>;
